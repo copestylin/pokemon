4c3486c1f1082e21e60abfc58772844d993ae7bb
John Nguyen <jtnguyen236@gmail.com>
2020-10-21 11:39:53 +0800

    add state management and routes (#23)
    Co-authored-by: John Nguyen <john@ninjasoftware.com.au>

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,25 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
+import { Dashboard } from './pages/Dashboard';
+import { Users } from './pages/UsersList';
+import { createContainer } from "unstated-next"
+import { Login } from './pages/Login';
+
+function useAppState(initialState = false) {
+  let [isLoggedIn, setIsLoggedIn] = useState(initialState)
+
+  let setLoggedInStatus = (val: boolean) => setIsLoggedIn(val)
+
+  return { isLoggedIn, setLoggedInStatus }
+}
+
+export let AppState = createContainer(useAppState)
+
+const Routes = () => {
+  let appState = AppState.useContainer()
+  if (!appState.isLoggedIn) {
+    return (<Router>
+      <div>
+        <button onClick={() => { appState.setLoggedInStatus(true) }}>Login</button>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+      renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </div>
+    </Router >)
+  }
+
+  return (<Router>
+    <div>
+      <nav>
+        <button onClick={() => { appState.setLoggedInStatus(false) }}>Logout</button>
+        <ul>
+          <li>
+            <Link to="/">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* A <Switch> looks through its children <Route>s and
+    renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/users">
+          <Users />
+        </Route>
+        <Route path="/">
+          <Dashboard />
+        </Route>
+      </Switch>
+    </div>
+  </Router>)
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppState.Provider>
+      <div className="App">
+        <Routes />
+      </div>
+    </AppState.Provider>
   );
 }
 
